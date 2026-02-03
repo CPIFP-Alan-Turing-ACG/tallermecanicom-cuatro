8650e024a38f1457ced53be60e3ede87cb7705b8
Jose509578 <jquialc2311@g.educaand.es>
2026-02-03 13:29:54 +0100

    commmmit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,9 @@ public class Main {
         // TODO: Alumno C debe inicializar aquí el inventario
 
         // TODO: Alumno B debe registrar una repar ación de prueba
-        
+        GestorReparaciones reparaciones = new GestorReparaciones();
+        reparaciones.procesarReparacion();
+
         System.out.println("Sistema detenido.");
     }
 }
