7b94b33b5fb9579115fc5bdd245f696702bb2017
Juan Caballero Castillo <jcabcas2806@g.educaand.es>
2026-02-03 13:26:35 +0100

    caca

--- src/GestorReparaciones.java
+++ src/GestorReparaciones.java
@@ -1,8 +1,7 @@
 public class GestorReparaciones {
- 
+
     public void procesarReparacion() {
-       System.out.println("Reanult 2004");
-       System.out.println("Se realizo un cambio de bateria Juan Perez Ortiz");
+        System.out.println("Reanult 2004");
+        System.out.println("Se realizo un cambio de bateria Juan Perez Ortiz");
     }
- }
- 
+}
