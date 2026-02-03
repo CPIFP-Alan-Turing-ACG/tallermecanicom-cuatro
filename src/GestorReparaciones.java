3f268ff1470f5dd2e12f38ee0ee16cf9655d8174
Jose509578 <jquialc2311@g.educaand.es>
2026-02-03 12:38:38 +0100

    commmit

--- src/GestorReparaciones.java
+++ src/GestorReparaciones.java
@@ -2,7 +2,7 @@ public class GestorReparaciones {
     public void procesarReparacion() {
         // TODO: Implementar lógica (Alumno B)
         System.out.println("Renault Megane 2004");
-        System.out.println("Se realizo un cambio de bateria   Antotio Ortiz Caballo");
+        System.out.println("Se realizo un cambio de bateria    Juan Ortiz Caballo");
 
     }
 }
